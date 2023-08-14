8517710da884ba7fac12db57ae62ca0348e29cc4
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-08-14 10:23:53 -0400

    Added app description, image and links to the website using pandas and column functions

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+# adding list in columns func to specify the ratio in terms of area covered in the webpage
+# since nothing is added in the empty col, no need to add another context manager.
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/photo.jpg")
@@ -16,18 +18,26 @@ with col2:
     Committed to personal and professional growth for a career in software engineering and AI. 
     """
     st.info(content)
-
-content2 = """ Below you can find some of the apps i have built in Python.
+# I am using markdown for info part to make it bolder
+content2 = """Below you can find some of the apps I have built in Python.
 Feel free to contact me!"""
-st.write(content2)
+st.markdown(f"**{content2}**")
 
-col3,col4 = st.columns(2)
+col3, col4 = st.columns(2)
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        # We are joining the path below from two diff directories for the images to be
+        # accessed and showed in the loop
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
