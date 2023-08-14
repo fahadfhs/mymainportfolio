bee0765337fa9d472b226ad0bf852813d3b5a63a
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-08-14 09:23:04 -0400

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg")#width=400)
+    st.image("images/photo.jpg")
 with col2:
     st.title("Fahad Shaikh")
     content = """Hi, I am Fahad Shaikh, Centennial College software engineering graduate, specializing in AI, 
@@ -15,3 +15,7 @@ with col2:
     Committed to personal and professional growth for a career in software engineering and AI. 
     """
     st.info(content)
+
+content2 = """ Below you can find some of the apps i have built in Python.
+Feel free to contact me!"""
+st.write(content2)
