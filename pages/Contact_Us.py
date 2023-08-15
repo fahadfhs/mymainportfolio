e359130a274fa6c498ec60e1c6c25c3abaadb210
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-08-15 13:30:05 -0400

    send emails using the form, connected both the files using imports. Also utilized the f string method for better readability while receiving emails

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
@@ -8,9 +9,16 @@ st.header("Contact Me")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     # text_area allows to enter multiline text
-    message = st.text_area("Type your message below")
+    raw_message = st.text_area("Type your message below")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     # button is boolean, hence when pressed it'll do the work below
     if button:
-        print("Pressed")
+        send_email(message)
+        st.info("Your email was sent successfully")
 
--- send_email.py
+++ send_email.py
@@ -1,21 +1,18 @@
-import smtplib, ssl
+import smtplib
+import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "fahadhussainshaikh1@gmail.com"
-password = "owwufqgrgzkkkjkh"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "fahadfhs95@gmail.com"
-context = ssl.create_default_context()
+    username = "fahadhussainshaikh1@gmail.com"
+    password = "owwufqgrgzkkkjkh"
 
-message = """\
-Subject: Check up
-Hi!
-How are you?
-Bye!
-"""
-# context = contex because we are being explicit, as the order of argument gets messed up
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    receiver = "fahadhussainshaikh1@gmail.com"
+    context = ssl.create_default_context()
+
+    # context = contex because we are being explicit, as the order of argument gets messed up
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
